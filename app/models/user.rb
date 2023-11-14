3e81b4756b3179dfa5b950ce1a3d5e9e7bf1d0dc
HaYeonKim00 <hayeon004@gmail.com>
2023-11-14 12:24:17 -0500

    user profile edit function - still need to work on data conflict(uni and id)

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
     has_secure_password
 
-    validates :uni, uniqueness: true 
+    # validates :uni, uniqueness: true 
     validates :password, confirmation: true
     validates :password_confirmation, presence: true
 
@@ -9,6 +9,17 @@ class User < ApplicationRecord
 
     # Generate a random verification code before creating the user
     before_create :generate_verification_code
+
+    enum gender: [:woman, :man, :transgender, :non_binary, :prefer_not_to_respond]
+    enum mbti: [:enfp, :enfj, :estj, :istj]
+
+    def self.gender_keys
+      genders.keys
+    end
+
+    def self.mbti_keys
+      mbtis.keys
+    end
   
     private
   
